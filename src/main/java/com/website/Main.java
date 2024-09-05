e0294fbc2126e996a33769373a334bfe050bd76a
Abhishek Ojha <aabhiojha08@outlook.com>
2024-09-05 08:31:16 +0545

    cryptographically strong generator

--- src/main/java/com/website/Main.java
+++ src/main/java/com/website/Main.java
@@ -1,24 +1,29 @@
 package com.website;
-import java.util.Random;
 
+import java.security.SecureRandom;
+import java.util.Random;
+//This class provides a cryptographically strong random number generator (RNG).
+// Better
 public class Main {
     private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
     private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final String DIGITS = "0123456789";
     private static final String SPECIAL_CHARS = "!@#$%^&*()-_=+[]{}|;:',.<>?/";
 
-    private static final String ALL_CHARS = LOWERCASE+UPPERCASE+DIGITS+SPECIAL_CHARS;
-    private static String password = new String();
-
-    private static String passwordGenerator(int length){
-        Random rd = new Random();
-        int a = rd.nextInt(ALL_CHARS.length());
-        password = ALL_CHARS.substring(a,a+length);
-        return password;
-    }
+    private static final String ALL_CHARS = LOWERCASE + UPPERCASE + DIGITS + SPECIAL_CHARS;
 
     public static void main(String[] args) {
-        System.out.println(passwordGenerator(10));
+        SecureRandom random = new SecureRandom();
+        String generatedPassword = "";
+        for (int i = 0; i <= 10; i++) {
+            int a = random.nextInt(ALL_CHARS.length());
+
+            //methods to join two strings
+            generatedPassword = generatedPassword.concat(Character.toString(ALL_CHARS.charAt(a)));
+            //        generatedPassword += ALL_CHARS.charAt(a);
+        }
+
+        System.out.println(generatedPassword);
     }
 }
 
--- src/main/java/com/website/secureGen/PasswordGenerator.java
+++ src/main/java/com/website/secureGen/PasswordGenerator.java
@@ -1,6 +1,25 @@
 package com.website.secureGen;
-import java.security.SecureRandom;
+import java.util.Random;
+// this approach is bad as it doesn't provide with actual cryptographically random numbers;
 
 public class PasswordGenerator {
-    SecureRandom random = new SecureRandom();
+    private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String DIGITS = "0123456789";
+    private static final String SPECIAL_CHARS = "!@#$%^&*()-_=+[]{}|;:',.<>?/";
+
+    private static final String ALL_CHARS = LOWERCASE+UPPERCASE+DIGITS+SPECIAL_CHARS;
+    private static String password = new String();
+
+    private static String passwordGenerator(int length){
+        Random rd = new Random();
+        int a = rd.nextInt(ALL_CHARS.length());
+        password = ALL_CHARS.substring(a,a+length);
+        return password;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(passwordGenerator(10));
+    }
 }
+
